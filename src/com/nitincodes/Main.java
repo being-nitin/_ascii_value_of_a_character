473f212edf317e6301bed650ed7f25653d4e6f3b
Nitin <nsinghboss02@gmail.com>
2021-09-16 18:58:31 +0530

    Initial commit

---
+++ src/com/nitincodes/Main.java
@@ -0,0 +1,18 @@
+package com.nitincodes;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	// java program to print ascii value of a character:
+    Scanner sc = new Scanner(System.in);
+        System.out.println("the character is");
+        char ch = sc.next().charAt(0);
+        int a = ch;
+        System.out.println("the ascii value of "+ch+" is" +a);
+
+
+
+
+
+    }
+}
